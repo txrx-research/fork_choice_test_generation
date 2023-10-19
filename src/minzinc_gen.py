dfde73e397b76630b807e1469fb75eace00fbdae
Alex Vlasov <avv49@mail.ru>
2023-10-19 19:17:22 +0400

    some comments added

--- src/minzinc_gen.py
+++ src/minzinc_gen.py
@@ -4,9 +4,9 @@ from minizinc import Instance, Model, Solver
 sm_links = Model('../minizinc/SM_links.mzn')
 gecode = Solver.lookup("gecode")
 instance = Instance(gecode, sm_links)
-instance['AE'] = 2
-instance['NE'] = 5
-instance['NL'] = 3
+instance['AE'] = 2 # anchor epoch
+instance['NE'] = 5 # number of epochs, starting from AE
+instance['NL'] = 3 # number of super-majority links
 
 results = instance.solve(all_solutions=True)
 for i in range(len(results)):
