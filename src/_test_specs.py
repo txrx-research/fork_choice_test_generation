1597cccbb304246fd2da3c6a8f68ce390ccfbb09
Alex Vlasov <avv49@mail.ru>
2023-12-04 19:14:31 +0400

    Refactoring

--- src/_test_specs.py
+++ src/_test_specs.py
@@ -26,233 +26,10 @@ from abstract_spec import (get_forkchoice_state, validate_tick, update_on_tick,
                            get_weight as abs_get_weight
                            )
 
+from test_case_builder import TC, TCBuilder, do_spec_step
+
 eth2spec.utils.bls.bls_active = False
 
-def compare_fc_state(fc_state: FCState, store: Store) -> None:
-    assert fc_state.time == store.time
-    assert fc_state.genesis_time == store.genesis_time
-    assert fc_state.get_justified_checkpoint() == store.justified_checkpoint, f"{fc_state.get_justified_checkpoint()} {store.justified_checkpoint}"
-    assert fc_state.get_finalized_checkpoint() == store.finalized_checkpoint
-    assert fc_state.proposer_boost_root == store.proposer_boost_root
-    assert fc_state.equivocating_indices == store.equivocating_indices
-    assert fc_state.blocks == set(store.blocks.values())
-    for block_root in store.block_states.keys():
-        assert fc_state.get_block_state(block_root) == store.block_states[block_root].freeze()
-    for checkpoint in store.checkpoint_states:
-        assert fc_state.get_checkpoint_state(checkpoint) == store.checkpoint_states[checkpoint].freeze()
-    assert fc_state.latest_messages == store.latest_messages
-    for block_root in store.unrealized_justifications.keys():
-        if block_root != fc_state.anchor_root:
-            assert fc_state.get_unrealized_justification(block_root) == store.unrealized_justifications[block_root]
-    assert abs_get_head(fc_state) == get_head(store)
-    assert abs_get_filtered_block_tree(fc_state) == get_filtered_block_tree(store)
-    for block_root in get_filtered_block_tree(store).keys():
-        assert abs_get_weight(fc_state, block_root) == get_weight(store, block_root)
-
-
-
-def do_spec_step(store, evt):
-    match evt:
-        case int(time):
-            on_tick(store, time)
-        case Attestation() as attestation:
-            on_attestation(store, attestation, False)
-        case SignedBeaconBlock() as signed_block:
-            on_block(store, signed_block)
-        case AttesterSlashing() as attester_slashing:
-            on_attester_slashing(store, attester_slashing)
-        case _:
-            assert False
-
-
-def do_abs_spec_step(fc_state, evt):
-    match evt:
-        case int(time):
-            validate_tick(fc_state, time)
-            fc_state = update_on_tick(fc_state, time)
-        case SignedBeaconBlock() as signed_block:
-            validate_block(fc_state, signed_block)
-            fc_state = update_on_block(fc_state, signed_block)
-        case Attestation() as attestation:
-            validate_attestation(fc_state, attestation, False)
-            fc_state = update_on_attestation(fc_state, attestation, False)
-        case AttesterSlashing() as attester_slashing:
-            validate_attester_slashing(fc_state, attester_slashing)
-            fc_state = update_on_attester_slashing(fc_state, attester_slashing)
-        case _:
-            assert False
-    return fc_state
-
-
-def mk_anchor():
-    no_validators = 16
-    anchor_state = genesis.create_genesis_state(phase0, [MAX_EFFECTIVE_BALANCE] * no_validators, 0)
-    anchor_block = BeaconBlock(state_root=hash_tree_root(anchor_state))
-    return anchor_state, anchor_block
-
-
-@dataclass(eq=True)
-class TC:
-    anchor: Tuple[BeaconState, BeaconBlock]
-    events: Sequence[int|SignedBeaconBlock|Attestation|AttesterSlashing]
-
-
-class TCBuilder:
-    def __init__(self):
-        self.anchor = mk_anchor()
-        self.events = []
-        self.store = get_forkchoice_store(*self.anchor)
-        self.attestations = []
-
-    def get_tc(self):
-        return TC(self.anchor, self.events)
-
-    def new_tick(self):
-        slot = get_current_slot(self.store)
-        tick = self.store.genesis_time + config.SECONDS_PER_SLOT*(slot - GENESIS_SLOT + 1)
-        do_spec_step(self.store, tick)
-        self.events.append(tick)
-
-    def _get_last_block(self):
-        return max(self.store.blocks.values(), key=lambda b: b.slot)
-
-    def _resolve_block_ref(self, ref):
-        if ref is None:
-            return get_head(self.store)
-        elif isinstance(ref, BeaconBlock):
-            return hash_tree_root(ref)
-        elif isinstance(ref, SignedBeaconBlock):
-            return self._resolve_block_ref(ref.message)
-        elif isinstance(ref, int):
-            for root, block in self.store.blocks.items():
-                if block.slot == ref:
-                    return root
-            assert False
-        else:
-            assert False
-
-    def send_block(self, signed_block):
-        do_spec_step(self.store, signed_block)
-        self.events.append(signed_block)
-
-    def mk_block(self, parent=None, atts=None):
-        parent_root = self._resolve_block_ref(parent)
-        slot = get_current_slot(self.store)
-        state = self.store.block_states[parent_root].copy()
-        block = block_helpers.build_empty_block(phase0, state, slot)
-        if atts is not None:
-            block.body.attestations.append(*atts)
-        else:
-            st = state.copy()
-            phase0.process_slots(st, slot)
-            selected_atts = []
-            for a in self.attestations:
-                assert isinstance(a, phase0.Attestation)
-                if (a.data.source == st.current_justified_checkpoint
-                            or a.data.source == st.previous_justified_checkpoint) \
-                        and (a.data.slot + phase0.MIN_ATTESTATION_INCLUSION_DELAY <= slot <= a.data.slot + phase0.SLOTS_PER_EPOCH):
-                    selected_atts.append(a)
-            for a in selected_atts:
-                block.body.attestations.append(a)
-
-
-        block = block_helpers.transition_unsigned_block(phase0, state, block)
-        block.state_root = hash_tree_root(state)
-        signed_block = block_helpers.sign_block(phase0, state, block)
-        return signed_block
-
-    def new_block(self, parent=None, atts=None):
-        self.send_block(self.mk_block(parent, atts))
-
-    # def mk_attestation(self, vals):
-    #     head = self._resolve_block_ref(None)
-    #     slot = get_current_slot(self.store)
-    #     epoch = compute_epoch_at_slot(slot)
-    #     source = self.store.justified_checkpoint
-    #     boundary_block = get_checkpoint_block(self.store, head, epoch)
-    #     data = AttestationData(
-    #         slot = slot, beacon_block_root=head, source=source,
-    #         target=Checkpoint(epoch=epoch, root=boundary_block))
-    #     return Attestation(data=data)
-
-    def mk_attestations(self):
-        head_root = self._resolve_block_ref(None)
-        slot = get_current_slot(self.store)
-        state = self.store.block_states[head_root].copy()
-        if state.slot < slot:
-            phase0.process_slots(state, slot)
-
-        block_root = head_root
-        current_epoch_start_slot = phase0.compute_start_slot_at_epoch(phase0.get_current_epoch(state))
-        if slot < current_epoch_start_slot:
-            epoch_boundary_root = phase0.get_block_root(state, phase0.get_previous_epoch(state))
-        elif slot == current_epoch_start_slot:
-            epoch_boundary_root = block_root
-        else:
-            epoch_boundary_root = phase0.get_block_root(state, phase0.get_current_epoch(state))
-
-        if slot < current_epoch_start_slot:
-            source_epoch = state.previous_justified_checkpoint.epoch
-            source_root = state.previous_justified_checkpoint.root
-        else:
-            source_epoch = state.current_justified_checkpoint.epoch
-            source_root = state.current_justified_checkpoint.root
-
-        attestation_data = phase0.AttestationData(
-            slot=slot,
-            index=0,
-            beacon_block_root=block_root,
-            source=phase0.Checkpoint(epoch=source_epoch, root=source_root),
-            target=phase0.Checkpoint(epoch=phase0.compute_epoch_at_slot(slot), root=epoch_boundary_root),
-        )
-
-
-        beacon_committee = phase0.get_beacon_committee(
-            state,
-            attestation_data.slot,
-            attestation_data.index,
-        )
-
-        committee_size = len(beacon_committee)
-        aggregation_bits = phase0.Bitlist[phase0.MAX_VALIDATORS_PER_COMMITTEE](*([0] * committee_size))
-        attestation = phase0.Attestation(
-            aggregation_bits=aggregation_bits,
-            data=attestation_data,
-        )
-        attestations_helper.fill_aggregate_attestation(phase0, state, attestation, signed=False, filter_participant_set=None)
-
-        atts = attestation
-        return atts
-
-    def send_attestation(self, att):
-        do_spec_step(self.store, att)
-        self.attestations.append(att)
-        self.events.append(att)
-
-
-def mk_initial_states(anchor):
-    anchor_state, anchor_block = anchor
-    store = get_forkchoice_store(anchor_state, anchor_block)
-    fc_state = get_forkchoice_state(anchor_state, anchor_block)
-    return fc_state, store
-
-
-def run_tc(tc: TC):
-    fc_state, store = mk_initial_states(tc.anchor)
-    compare_fc_state(fc_state, store)
-    for evt in tc.events:
-        do_spec_step(store, evt)
-        fc_state = do_abs_spec_step(fc_state, evt)
-        compare_fc_state(fc_state, store)
-
-
-def enumerate_stores(tc: TC) -> Sequence[Store]:
-    anchor_state, anchor_block = tc.anchor
-    store = get_forkchoice_store(anchor_state, anchor_block)
-    yield store
-    for e in tc.events:
-        do_spec_step(store, e)
-        yield store
 
 
 def calc_block_predicate_coverage(store: Store, block_root):
@@ -301,30 +78,31 @@ def _test_anchor_state():
 
 def _test_simple_tick():
     builder = TCBuilder()
-    builder.new_tick()
+    builder.new_slot()
     return builder.get_tc()
 
 
 def _test_on_attestation():
     builder = TCBuilder()
     atts = builder.mk_attestations()
-    builder.new_tick()
+    builder.new_slot()
     builder.send_attestation(atts)
     return builder.get_tc()
 
 
 def _test_on_attester_slashing():
+    builder = TCBuilder()
     data_1 = AttestationData(0, Checkpoint(0, 0), Checkpoint(2, 0), 0)
     data_2 = AttestationData(0, Checkpoint(1, 0), Checkpoint(2, 0), 0)
     attester_slashing = AttesterSlashing(IndexedAttestation(data_1, (1,2)), IndexedAttestation(data_2, (2,3)))
-    return TC(mk_anchor(), [attester_slashing])
+    return TC(builder.anchor, [attester_slashing])
 
 
 def _test_on_block():
     builder = TCBuilder()
     for i in range(31):
         atts = builder.mk_attestations()
-        builder.new_tick()
+        builder.new_slot()
         builder.send_attestation(atts)
         builder.new_block(atts=(atts,))
     return builder.get_tc()
@@ -339,9 +117,6 @@ tcs = []
 #tcs.append(_test_on_block())
 
 
-from minzinc_gen import generate_sm_link_test_cases
-
-cases = [{'sources':[0,0,0,0],'targets':[1,2,3,4]}]
 # for case in cases: #generate_sm_link_test_cases():
 #     sources = case['sources']
 #     targets = case['targets']
@@ -360,58 +135,6 @@ cases = [{'sources':[0,0,0,0],'targets':[1,2,3,4]}]
 #         builder.send_attestation(atts)
 #         builder.new_block(parent=src*phase0.SLOTS_PER_EPOCH, atts=None)
 #     tcs.append(builder.get_tc())
-
-builder = TCBuilder()
-for i in range(15):
-    atts = builder.mk_attestations()
-    builder.new_tick()
-    builder.send_attestation(atts)
-
-atts_prev = builder.mk_attestations()
-builder.new_tick()
-# 0
-builder.send_attestation(atts_prev)
-atts0 = builder.mk_attestations()
-builder.new_tick()
-# 1
-builder.send_attestation(atts0)
-atts1 = builder.mk_attestations()
-builder.new_tick()
-# 2
-builder.send_attestation(atts1)
-builder.new_block()
-atts2 = builder.mk_attestations()
-builder.new_tick()
-# 3
-builder.send_attestation(atts2)
-builder.new_block()
-atts3 = builder.mk_attestations()
-builder.new_tick()
-# 4
-builder.send_attestation(atts3)
-builder.new_block()
-atts4 = builder.mk_attestations()
-builder.new_tick()
-# 5
-builder.send_attestation(atts4)
-builder.new_block()
-atts5 = builder.mk_attestations()
-builder.new_tick()
-# 6
-builder.send_attestation(atts5)
-builder.new_block()
-atts6 = builder.mk_attestations()
-builder.new_tick()
-# 7
-builder.send_attestation(atts6)
-builder.new_block()
-atts7 = builder.mk_attestations()
-builder.new_tick()
-# 8
-
-
-print()
-
 
 # block_pred_cov = set()
 # for tc in tcs:
